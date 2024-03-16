631c5ec1ea8323c2f129cb9dc93e5131ece21317
Your Name~ <you@example.com>
2024-03-16 20:13:29 -0300

    test vscode googlearch

--- tsgoogle.py
+++ tsgoogle.py
@@ -1,16 +1,7 @@
-from googleapiclient.discovery import build
+from googlesearch import search
 
-API_KEY = 'sua_chave_de_api'
-CX_ID = 'seu_id_da_pesquisa_personalizada'
+query = "loja ferramentas mercadoshops"
+num_results = 10
 
-def google_custom_search(query, num_results=10):
-    service = build('customsearch', 'v1', developerKey=API_KEY)
-    res = service.cse().list(q=query, cx=CX_ID, num=num_results).execute()
-
-    for item in res['items']:
-        print(f"Resultado: {item['title']} - {item['link']}")
-
-query = input("Digite o termo de pesquisa: ")
-num_results = int(input("Quantidade de resultados desejados: "))
-
-google_custom_search(query, num_results)
+for result in search(query, num_results=num_results):
+    print(result)
